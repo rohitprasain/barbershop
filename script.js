98d39e699a83b5ecab893758c9c4fbaf0d76e8b7
rohit <rohit@pop-os.localdomain>
2021-06-25 19:41:01 +0545

    finished header

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var closeImg = document.getElementsByClassName('logo');
+var sideNav = document.getElementsByClassName('sidenav');
+
+
+closeImg.onClick = function(){
+    alert()
+    // sideNav.display ='none';
+
+}
